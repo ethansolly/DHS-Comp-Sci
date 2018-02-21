d6d78449b2946d4c183241f0613211648bae289e
Lutalica <32465865+ethansolly@users.noreply.github.com>
2018-02-20 22:07:01 -0600

    Update Main.java
    Removed init method, set bounds for graphing

--- Complex/Main.java
+++ Complex/Main.java
@@ -5,18 +5,19 @@ import java.util.function.Function;
 
 public class Main extends Applet {
 
-    private final Function<Complex, Complex> function = Complex::cos;
+    private final Function<Complex, Complex> function = Complex::erica;
     private static final Color[] palette = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE};
 
-    public void init() {
-        setSize(Toolkit.getDefaultToolkit().getScreenSize());
-    }
+    double x1 = -1;
+    double x2 = 1;
+    double y1 = -1;
+    double y2 = 1;
 
     public void paint(Graphics g) {
         for(int i = 0; i < getWidth(); i+=1) {
             for (int j = 0; j < getHeight(); j+=1) {
-                double x = 2*((double)i/getWidth())-1;
-                double y = 2*((double)j/getHeight())-1;
+                double x = x2*((double)i/getWidth())+x1*(1.0-(double)i/getWidth());
+                double y = y2*((double)j/getHeight())+y1*(1.0-(double)j/getHeight());
                 Complex c = function.apply(new Complex(x, y));
                 //System.out.println("( " + x + ", " + y + ") => " + "( " + c.a + ", " + c.b + ")");
                 g.setColor(c.getColor());

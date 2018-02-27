599fdbab37330b3302c2d1817d153beb48d0fc71
Lutalica <ethansolly@gmail.com>
2018-02-27 14:21:42 -0600

    Draggable graph

--- Complex/Main.java
+++ Complex/Main.java
@@ -1,23 +1,30 @@
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.function.Function;
 
-public class Main extends Applet {
+public class Main extends Applet implements MouseListener {
 
     boolean useT = false;
     double t;
 
-    private final ComplexFunction function = ComplexFunction.GAMMA;
+    private final ComplexFunction function = new ComplexFunction(c -> c.pow(-1));
     private static final Color[] palette = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE};
 
-    double x1 = -10;
-    double x2 = 1;
-    double y1 = -1;
-    double y2 = 1;
+    double x1 = -3;
+    double x2 = 3;
+    double y1 = -3;
+    double y2 = 3;
 
     double t1 = -10;
     double t2 = 10;
     double tInc = 0.01;
+
+    public void init() {
+        addMouseListener(this);
+    }
 
     public void paint(Graphics g) {
         if (useT) {
@@ -42,5 +49,44 @@ public class Main extends Applet {
                 g.drawRect(i, j, 1, 1);
             }
         }
+    }
+
+    private boolean pressed = false;
+    private int dx;
+    private int dy;
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        pressed = true;
+        dx = e.getX();
+        dy = e.getY();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        dx -= e.getX();
+        dy -= e.getY();
+
+        x1+=dx*(x2-x1)/getWidth();
+        x2+=dx*(x2-x1)/getWidth();
+
+        y1+=dy*(y2-y1)/getHeight();
+        y2+=dy*(y2-y1)/getHeight();
+        repaint();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }

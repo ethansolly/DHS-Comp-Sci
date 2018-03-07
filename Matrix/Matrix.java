d5dc3708aebf09b5a4aab589d2fd3dd45fce1ed8
Lutalica <ethansolly@gmail.com>
2018-03-07 14:44:55 -0600

    aaaaaa

--- Matrix/Matrix.java
+++ Matrix/Matrix.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Matrix {
 
-    private int[][] matr;
+    private double[][] matr;
 
     public static Matrix identity(int n) {
         Matrix temp = new Matrix(n, n);
@@ -14,10 +14,10 @@ public class Matrix {
         return temp;
     }
 
-    public Matrix(int n) { matr = new int[n][n]; }
+    public Matrix(int n) { matr = new double[n][n]; }
 
     public Matrix(int r, int c) {
-        matr = new int[r][c];
+        matr = new double[r][c];
     }
 
     public int getR() {
@@ -28,7 +28,7 @@ public class Matrix {
         return matr[0].length;
     }
 
-    public boolean setVal(int val, int r, int c) {
+    public boolean setVal(double val, int r, int c) {
         if (r < getR() && c < getC()) {
             matr[r][c] = val;
             return true;
@@ -51,6 +51,16 @@ public class Matrix {
         else throw new UnsupportedOperationException("Matrices must have an equal number of rows and columns in order to add them.");
     }
 
+    public Matrix multiply(double d) {
+        Matrix ret = clone();
+        for (int i = 0; i < getR(); i++) {
+            for (int j = 0; j < getC(); j++) {
+                ret.setVal(matr[i][j]*d, i, j);
+            }
+        }
+        return ret;
+    }
+
     public Matrix multiply(Matrix m) throws UnsupportedOperationException {
         if (getC() == m.getR()) {
             Matrix temp = new Matrix(getR(), m.getC());
@@ -66,6 +76,72 @@ public class Matrix {
         else throw new UnsupportedOperationException("Matrix A must have the same numer of columns as matrix B has rows!");
     }
 
+    public double det() {
+        if (getR() == getC()) {
+            int n = getR();
+            if (n == 1) {
+                return matr[0][0];
+            }
+            else if (n == 2) {
+                return matr[0][0]*matr[1][1] - matr[1][0]*matr[0][1];
+            }
+            else {
+                double ret = 0;
+                for (int i = 0; i < n; i++) {
+                    Matrix temp = new Matrix(n-1, n-1);
+
+
+                    for (int j = 1; j < n; j++) {
+                        for (int k = 0; k < n; k++) {
+                            if (k < i)
+                                temp.setVal(matr[j][k], j-1, k);
+                            else if (k > i)
+                                temp.setVal(matr[j][k], j-1,k-1);
+                        }
+                    }
+
+                    ret += matr[0][i]*Math.pow(-1, (double)i)*temp.det();
+
+                }
+                return ret;
+            }
+        }
+        else return Double.NaN;
+    }
+
+    public double trace() throws UnsupportedOperationException {
+        if (getR() == getC()) {
+            double ret = 0;
+            int n = getR();
+            for (int i = 0; i < n; i++) {
+                ret += matr[i][i];
+            }
+            return ret;
+        }
+        else throw new UnsupportedOperationException("Trace cannot be applied to non-square matrices.");
+    }
+
+    public Matrix inverse() throws UnsupportedOperationException {
+        if (getR() == getC() && det() != 0) {
+            int n = getR();
+            Matrix ret = new Matrix(n, n);
+            if (n == 1) {
+                ret.setVal(1.0/matr[0][0], 0, 0);
+            }
+            else if (n == 2) {
+                ret = identity(2).multiply(trace()).add(multiply(-1)).multiply(1.0/det());
+            }
+            else if (n == 3) {
+                ret = identity(3).multiply((trace()*trace()-multiply(this).trace())/2.0).add(multiply(-trace())).add(multiply(this)).multiply(1.0/det());
+            }
+            else if (n == 4) {
+
+            }
+            return ret;
+        }
+        else throw new UnsupportedOperationException("Non-square matrices cannot be inverted.");
+    }
+
     public String toString() {
         String out = "";
         for (int i = 0; i < getR(); i++) {
@@ -75,6 +151,12 @@ public class Matrix {
             out += "\n";
         }
         return out;
+    }
+
+    public Matrix clone() {
+        Matrix ret = new Matrix(getR(), getC());
+        ret.matr = matr;
+        return ret;
     }
 
 }
--- Matrix/MatrixMain.java
+++ Matrix/MatrixMain.java
@@ -6,13 +6,13 @@ public class MatrixMain {
         Random r = new Random();
         int n = r.nextInt(5)+1;
         Matrix m = new Matrix(n);
-        /*for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int set = r.nextInt(3)-1;
                 m.setVal(set, i, j);
             }
-        }*/
+        }
         System.out.println(m);
-        System.out.println(m.multiply(m));
+        System.out.println(m.det());
     }
 }

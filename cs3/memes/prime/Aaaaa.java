64348d59f3c3ab1208999276059c828d28960694
Lutalica <ethansolly@gmail.com>
2018-04-09 23:59:05 -0500

    More L's

--- cs3/memes/prime/Aaaaa.java
+++ cs3/memes/prime/Aaaaa.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.ImageObserver;
+import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.text.AttributedCharacterIterator;
 import java.util.*;
@@ -14,61 +15,34 @@ import java.util.List;
 import java.util.function.Consumer;
 
 import com.sun.jmx.remote.internal.ArrayQueue;
+import memes.perdono.Utilz;
 import memes.prime.Aaaaa.Alphabet;
 
 import static memes.prime.Aaaaa.Alphabet.*;
 
-public class Aaaaa extends JApplet implements MouseListener {
+public class Aaaaa extends JApplet{
 
 
     private ArrayList<Alphabet> state = new ArrayList<>();
 
     private double X;
     private double Y;
-    private int dx;
-    private int dy;
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        dx = e.getX();
-        dy = e.getY();
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        dx -= e.getX();
-        dy -= e.getY();
-
-        X-=dx;
-        Y-=dy;
-
-        repaint();
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
 
     enum Alphabet {
+        w,
         x,
         y,
+        z,
+
         f,
         g,
+
         p,
         m,
+
         l,
         r,
+
         c0,
         c1,
         c2,
@@ -78,19 +52,18 @@ public class Aaaaa extends JApplet implements MouseListener {
 
     public void init() {
 
-        X = getWidth()/2.0;
-        Y = getHeight()/2.0;
+        setSize(Utilz.screen);
 
-        lSystem = LSystem.SIERPINSKI;
+        X = getWidth()/2;
+        Y = getHeight()/2;
 
-        addMouseListener(this);
+        lSystem = LSystem.LACE;
 
-        int n = 6;
+        int n = 7;
 
         //Axiom
         List axiom = lSystem.getAxiom();
         state.addAll(axiom);
-
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < state.size(); j++) {
@@ -107,18 +80,22 @@ public class Aaaaa extends JApplet implements MouseListener {
         }
     }
 
+    boolean repaint = false;
+
     public void paint(Graphics g) {
+
+        ArrayList<Alphabet> stateClone = new ArrayList<>(state);
 
         double baseAngle = lSystem.getBaseAngle();
         double angle = 0;
 
-        double mag = 10;
+        double mag = 3;
 
         Stack<Double> savedX = new Stack<>();
         Stack<Double> savedY = new Stack<>();
         Stack<Double> savedAngles = new Stack<>();
 
-        for (Alphabet a: state) {
+        for (Alphabet a: stateClone) {
             switch (a.name()) {
                 case "g" :
                 case "f" :
@@ -176,9 +153,30 @@ class LSystem {
 
     private static HashMap SIERPINSKY_MAP = new HashMap<>();
     public static final LSystem SIERPINSKI = new LSystem(
-            Arrays.asList(f, m, g, m, g),
+            Arrays.asList(f),
             SIERPINSKY_MAP,
             2.0*Math.PI/3.0
+    );
+
+    private static HashMap KOCH_MAP = new HashMap<>();
+    public static final LSystem KOCH = new LSystem(
+            Arrays.asList(f),
+            KOCH_MAP,
+            Math.PI/2.0
+    );
+
+    private static HashMap CROSS_MAP = new HashMap<>();
+    public static final LSystem CROSS = new LSystem(
+            Arrays.asList(x,y,x,y,x,y,x,p,x,y,x,y,x,y,x,p,x,y,x,y,x,y,x,p,x,y,x,y,x,y,x),
+            CROSS_MAP,
+            Math.PI/2.0
+    );
+
+    private static HashMap LACE_MAP = new HashMap<>();
+    public static final LSystem LACE = new LSystem(
+            Arrays.asList(w),
+            LACE_MAP,
+            Math.PI/6.0
     );
 
 
@@ -188,8 +186,19 @@ class LSystem {
 
         KEVS_TREE_MAP.put(f, Arrays.asList(c0, f, f, m, l, c1, m, f, p, f, p, f, r, p, l, c2, p, f, m, f, m, f, r));
 
-        SIERPINSKY_MAP.put(f, Arrays.asList(f,m,g,p,f,p,g,m,f));
-        SIERPINSKY_MAP.put(g, Arrays.asList(g,g));
+        SIERPINSKY_MAP.put(f, Arrays.asList(g, m, f, m, g));
+        SIERPINSKY_MAP.put(g, Arrays.asList(f, p, g, p, f));
+
+        KOCH_MAP.put(f, Arrays.asList(f,p,f,m,f,m,f,p,f));
+
+        CROSS_MAP.put(f, Arrays.asList());
+        CROSS_MAP.put(x, Arrays.asList(f,x,p,f,x,p,f,x,f,y,m,f,y,m));
+        CROSS_MAP.put(y, Arrays.asList(p,f,x,p,f,x,f,y,m,f,y,m,f,y));
+
+        LACE_MAP.put(w, Arrays.asList(p,p,p,x,m,m,f,m,m,z,f,x,p));
+        LACE_MAP.put(x, Arrays.asList(m,m,m,w,p,p,f,p,p,y,f,w,m));
+        LACE_MAP.put(y, Arrays.asList(p,z,f,x,m,m,f,m,m,z,p,p,p));
+        LACE_MAP.put(z, Arrays.asList(m,y,f,w,p,p,f,p,p,y,m,m,m));
     }
 
 
